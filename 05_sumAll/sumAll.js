9a84aeb1d239c37d98f1a3de8c2844b213240d66
justinthdang <justinthdang@gmail.com>
2024-05-07 21:39:40 -0400

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    if (int1 > int2) {
+        let newInt = int1;
+        int1 = int2;
+        int2 = newInt;
+    }
+
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    for(i = int1; i <= int2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
